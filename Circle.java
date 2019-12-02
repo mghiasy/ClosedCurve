011cdb0ba62d6b623d964880d2e02e8f604c1e53
Maryam Ghiasvand <m.ghiyasvand001@gmail.com>
2019-12-02 00:17:03 -0600

    create class without inheritace, use INSTANCE OF method

--- Circle.java
+++ Circle.java
@@ -2,13 +2,13 @@ package lab2.prog2_2;
 
 //FINLA CLASS CANNOT BE INHERITED
 
-public final class Circle{
+public final class Circle {
 	private final double radius;
 
 	public Circle(double radius) {
 		this.radius = radius;
 	}
-	
+
 	public String computeArea() {
 		double area = Math.PI * radius * radius;
 		return "Area of Circle is " + area;
